e175cc80f4334898becc3120e0e84f5f7a47cc0c
veronika67676 <nikolaeva.veronika688@gmail.com>
2025-04-26 14:57:36 +0300

    Add files via upload
    update

---
+++ SnakeGame-main/game/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("game")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ebe3ff7d-9783-4d75-b45d-0804027a997b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SnakeGame-main/snake/snake/Authorification.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snake
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorification.xaml
+    /// </summary>
+    public partial class Authorification : Page
+    {
+        private const string predefinedLogin = "snake";
+        private const string predefinedPassword = "1234";
+        public Authorification()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginTextBox.Text;
+            var password = PasswordBox.Password;
+
+            if (login == predefinedLogin && password == predefinedPassword)
+            {
+                StatusTextBlock.Text = "Успешная авторизация!";
+                StatusTextBlock.Foreground = System.Windows.Media.Brushes.Green;
+
+                MainWindow nigger = new MainWindow();
+                nigger.Show();
+
+                
+            }
+            else
+            {
+                StatusTextBlock.Text = "Логин или пароль неверны.";
+                StatusTextBlock.Foreground = System.Windows.Media.Brushes.Red;
+            }
+        }
+    }
+}
